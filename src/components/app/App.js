595d8f3e26b9e8f293f19ce4765aa7f368785588
mangupli <elizaveta.mangupli@gmail.com>
2022-10-13 20:53:56 +0300

    functionality of choosing currency and ongoing converting

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,48 +1,82 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ConverterService from "../../services/ConverterService";
 
 const App = (props) => {
-    const [value, setValue] = useState(props.amount);
+	const [value, setValue] = useState(props.amount);
+	const [result, setResult] = useState(null);
+	const [currency, setCurrency] = useState(null);
 
-    const [result, setResult] = useState(null);
+	const converterService = new ConverterService();   
+	
+	let activeButton = useRef(null);
 
-    const converterService = new ConverterService();    
+	useEffect(()=>{
+	  activeButton.current.classList.add('controls__button_selected');
+	});
 
-    function onClick(currency){
-        converterService.convert('RUB', currency, value)
-        .then(res => {
-            console.log(res);
-            setResult(res.result.toFixed(3));
-        })
-        .catch(error => console.log(error))
-    }
+	function onClick(e, currency){
+/*         converterService.convert('RUB', currency, value)
+		.then(res => {
+			console.log(res);
+			setResult(res.result.toFixed(3));
+		})
+		.catch(error => console.log(error)) */
+		if(e.target){
+		  if(activeButton.current){
+			activeButton.current.classList.remove('controls__button_selected');
+		  }
+		  activeButton.current = e.target;
+		  setCurrency(e.target.getAttribute('data-currency'));
+		  e.target.classList.add('controls__button_selected');
+		}
+	}
 
-    function onValueChange(e){
-        setValue(e.target.value);
-        setResult(null);
-    }
+	const onValueChange = (e) =>{
+		setValue(e.target.value);
+		setResult(null);
+	}
 
-    function reset(){
-        setResult(null);
-        setValue(0);
+	useEffect(()=>{
+		converterService.convert('RUB', currency, value)
+		.then(res => {
+			console.log(res);
+			setResult(res.result.toFixed(3));
+		})
+		.catch(error => console.log(error))
+	}, [value, currency]);
 
-    }
+	function reset(){
+		setResult(null);
+		setValue(0);
+	}
    
-    return (
-      <div className="app">
-        
-        <div className="input">
-            <input type="number" value={value} onChange={onValueChange}/>
-        </div>
-        <div className="counter">{result}</div>
-        <div className="controls">
-          <button className="controls__button" onClick={()=>onClick('USD')}>USD</button>
-          <button className="controls__button" onClick={()=>onClick('EUR')}>EUR</button>
-          <button className="controls__button" onClick={()=>onClick('INR')}>INR</button>
-          <button className="controls__button" onClick={reset}>RESET</button>
-        </div>
-      </div>
-    )
+	return (
+	  <div className="app">
+		
+		<div className="input">
+			<input type="number" value={value} onChange={onValueChange}/>
+		</div>
+		<div className="counter">{result}</div>
+		<div className="controls">
+		  <button
+		  	ref={activeButton}
+			className="controls__button"
+			onClick={(e)=>onClick(e, 'USD')}
+			data-currency="USD">USD</button>
+		  <button
+		  	className="controls__button"
+			onClick={(e)=>onClick(e, 'EUR')}
+			data-currency="EUR">EUR
+			</button>
+		  <button
+		  	className="controls__button"
+			onClick={(e)=>onClick(e, 'INR')}
+			data-currency="INR"
+			>INR</button>
+		  <button className="controls__button" onClick={reset}>RESET</button>
+		</div>
+	  </div>
+	)
   }
 
   export default App;
